122e335e374709a7c8c317ffb748374440df8ef3
DEVROGERZEN <devrogerzen@gmail.com>
2022-12-26 11:27:43 -0300

    form contact init

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,9 @@ export function Navbar({href,  text}) {
   return (
     <nav className= {'navbar_home'}>
       <ActiveLink href='/' text="Home"></ActiveLink>
-      <ActiveLink href='/about' text="About"></ActiveLink>
-      <ActiveLink href='/contents' text="Contents"></ActiveLink>
-      <ActiveLink href='/contact' text="Contact"></ActiveLink>
+      <ActiveLink href='/about' text="Quien Soy"></ActiveLink>
+      <ActiveLink href='/contents' text="Contenido"></ActiveLink>
+      <ActiveLink href='/contact' text="Contacto"></ActiveLink>
     </nav>
   )
 }
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,10 +1,70 @@
-import Head from "next/head";
+import React, { useRef } from "react";
+import Router from "next/router";
+import emailjs from "@emailjs/browser";
 import { MainLayout } from "../components/layouts/MainLayout";
+import Swal from "sweetalert2";
+const redirectTo = "/";
 
-export default function contact() {
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ysh1e6l",
+        "template_tsrzecv",
+        form.current,
+        "F4qdV9AuCwVIQHSza"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+
+          Swal.fire({
+            title: "Enviado!",
+            text: `Recibido 
+            ${usuarioform.name} Gracias por tu mensaje te respondere a
+            ${usuarioform.email} en la brevedad posible`,
+            icon: "success",
+            confirmButtonText: "Aceptar",
+          });
+
+          Router.push(redirectTo)
+
+          /* accion
+          swall con animacion 
+          link a inicio o a alguna pagina  */
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    let usuarioform = {
+      name: e.target.user_name.value,
+      email: e.target.user_email.value,
+      message: e.target.message.value,
+    };
+
+    console.log(usuarioform);
+  };
+
   return (
     <MainLayout>
-      <h1>I am /Contact Page</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Nombre</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Mensaje</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </MainLayout>
   );
-}
+};
+
+export default ContactUs
